392f62cb30e6d6bc8e7dfbf3403bfa2ada36c485
Jessie Hao <juan.hao@nxp.com>
2017-10-19 15:17:14 +0800

    MA-10492-2 Export UseSwOpengl to soong.
    Only take effect when HAVE_FSL_IMX_GPU3D=false in BoardConfig.mk.
    
    Change-Id: Ib7355a79b9c6c868ff4f48ad6c968c5ffb5ff1ff
    Signed-off-by: juan.hao <juan.hao@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/3104
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -82,6 +82,7 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "IntegerOverflowExcludePaths": $(call json_list,$(INTEGER_OVERFLOW_EXCLUDE_PATHS) $(PRODUCT_INTEGER_OVERFLOW_EXCLUDE_PATHS)),'; \
 	echo '    "Device_uses_hwc2": $(if $(filter true,$(TARGET_USES_HWC2)),true,false),'; \
 	echo '    "Override_rs_driver": "$(OVERRIDE_RS_DRIVER)",'; \
+	echo '    "UseSwOpengl": $(if $(filter false,$(HAVE_FSL_IMX_GPU3D)),true,false),'; \
 	echo '    "Treble": $(if $(filter true,$(PRODUCT_FULL_TREBLE)),true,false),'; \
 	echo '    "Pdk": $(if $(filter true,$(TARGET_BUILD_PDK)),true,false),'; \
 	echo ''; \
