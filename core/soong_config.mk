8dea0c3fb8be1a2fe87c82dfeaf7a4371fba25f8
Jessie Hao <juan.hao@nxp.com>
2018-09-03 16:24:07 +0800

    MA-12363 add UseSwOpengl, Hantro_vpu, Chipsmedia_vpu, Imx6dq_soc, Malone_vpu product variable
    This patch merge below 4 commits in maddev_or8.1 branch:
    
    c116597 MA-11706 add malone vpu variable
    553906b ENGR00325098 add imx6dq_soc product variable
    cc5d0b3 MA-10478 Export hantro_vpu and chipsmedia_vpu to soong
    c1b396d MA-10492-2 Export UseSwOpengl to soong.
    
    Change-Id: I33962f37a82ba0592b3d0edc2980cb5230b2da1a
    Signed-off-by: Jessie Hao <juan.hao@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/5803
    Reviewed-by: mao xu <mao.xu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/6359

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -82,6 +82,11 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "IntegerOverflowExcludePaths": $(call json_list,$(INTEGER_OVERFLOW_EXCLUDE_PATHS) $(PRODUCT_INTEGER_OVERFLOW_EXCLUDE_PATHS)),'; \
 	echo '    "Device_uses_hwc2": $(if $(filter true,$(TARGET_USES_HWC2)),true,false),'; \
 	echo '    "Override_rs_driver": "$(OVERRIDE_RS_DRIVER)",'; \
+	echo '    "UseSwOpengl": $(if $(filter false,$(HAVE_FSL_IMX_GPU3D)),true,false),'; \
+	echo '    "Hantro_vpu": $(if $(filter hantro,$(BOARD_VPU_TYPE)),true,false),'; \
+	echo '    "Chipsmedia_vpu": $(if $(filter chipsmedia,$(BOARD_VPU_TYPE)),true,false),'; \
+	echo '    "Malone_vpu": $(if $(filter malone,$(BOARD_VPU_TYPE)),true,false),'; \
+	echo '    "Imx6dq_soc": $(if $(filter IMX6DQ,$(BOARD_SOC_TYPE)),true,false),'; \
 	echo '    "Treble": $(if $(filter true,$(PRODUCT_FULL_TREBLE)),true,false),'; \
 	echo '    "Pdk": $(if $(filter true,$(TARGET_BUILD_PDK)),true,false),'; \
 	echo ''; \
